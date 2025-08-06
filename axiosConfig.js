d9c8a11f22097dbe0dec20762fb661c879fec6a0
TASh7899 <tash64934@gmail.com>
2025-08-06 13:15:26 +0530

    fixed axios

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  console.log("Calling API:", import.meta.env.VITE_API_URL);
   baseURL: `${process.env.REACT_APP_API_URL || "http://localhost:3000"}`,
   withCredentials: true,
 });
